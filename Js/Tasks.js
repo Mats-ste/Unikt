9da782cc2306b91af239a1636ca4170d7c467c79
Mats-ste <mms080607@gmail.com>
2024-12-09 09:24:12 +0100

    neie nei nei

--- Js/Tasks.js
+++ Js/Tasks.js
@@ -1,28 +1,3 @@
-document.getElementById("addTaskButton").addEventListener("click", addTask)
-document.getElementById("removeTaskButton").addEventListener("click", removeTask)
-
-function addTask() {
-let task = document.getElementById("taskInput").value;
-let listItem = document.createElement("li")
-listItem.textContent = task
-document.getElementById("taskList").appendChild(listItem)
-document.getElementById("taskInput").value = "";
-
-}
-
-function removeTask(){
-    let task = document.getElementById("taskInput").value;
-    let tasklist = document.getElementById("taskList");
-    let listItems = tasklist.getElementsByTagName("li");
-
-    for(let i = 0; i < listItems.length; i++){
-        if(listItems[i].textContent === task) {
-            tasklist.removeChild(listItems[i]);
-            return
-
-        }
-
-    }
-
-
-}
+const x = 10;
+x = 20;
+console.log(x)
